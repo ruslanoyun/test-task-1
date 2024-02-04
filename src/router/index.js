d3526bff360635395b07e6685456e27ef9d09b02
ruslanoyun <careerRuslanOyun@gmail.com>
2024-02-04 17:26:23 +0300

    Created new component

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import HelloWorld from '@/components/HelloWorld';
+import UserListPage from '@/components/UserListPage';
 
 Vue.use(Router);
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld,
+    },
+    {
+      path: '/list',
+      name: 'UserListPage',
+      component: UserListPage,
     },
   ],
 });
